881da9f159fa2ebfb330c7597c8f0781b8cf0b38
Prathamesh Pawar <prathamesh.pawar1000@gmail.com>
2023-04-17 01:41:32 -0400

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -1,113 +1,128 @@
-
 import re
-
 from rouge import Rouge
-# Initialize ROUGE
-rouge = Rouge()
-
 from transformers import pipeline
 from datasets import load_dataset
-
 from huggingface_hub import notebook_login
-notebook_login()
 
-data_files = {"test": "1000_test.json"}
+def main():
+    # Initialize ROUGE
+    rouge = Rouge()
+
+    # Login to Hugging Face Hub
+    notebook_login()
+
+    data_files = {"test": "1000_test.json"}
+
+    dataset = load_dataset("PrathameshPawar/summary_2k", data_files=data_files)
+
+    # Bart-Base model shall be used as a control summary to evaluate the score against
+    summarizer = pipeline("summarization", model="facebook/bart-base")
+    bart_base_summary = get_chunks(dataset['test']['content'][0], summarizer)
+    rouge_score_generation(dataset['test']['summary'][0], bart_base_summary)
+
+    # We concluded that Bart-base model finetuned on custom preprocessing data approach as the best performing model
+    summarizer = pipeline("summarization", model="PrathameshPawar/bart_custom")
+    bart_custom_summary = get_chunks(dataset['test']['custom_approach'][0], summarizer)
+    rouge_score_generation(dataset['test']['summary'][0], bart_custom_summary)
+
+    # Pegasus model shall be used as a control summary to evaluate the score against
+    summarizer = pipeline("summarization", model="google/pegasus-arxiv")
+    pegasus_base_summary = get_chunks(dataset['test']['content'][0], summarizer)
+    rouge_score_generation(dataset['test']['summary'][0], pegasus_base_summary)
+
+    # We concluded that Pegasus model finetuned on custom preprocessing data approach as the best performing model
+    summarizer = pipeline("summarization", model="PrathameshPawar/pegasus_custom",)
+
+    pegasus_custom_summary = get_chunks(dataset['test']['custom_approach'][0])
+
+    rouge_score_generation(dataset['test']['summary'][0],pegasus_custom_summary)
 
 
-def split_into_chunks(text, max_length):
-    """
-    Splits a string into chunks of text with complete sentences, where each chunk
-    has a maximum length of `max_length` characters.
-    """
-    sentences = re.findall(r'[^\n.!?]+[.!?]', text)  # Split into sentences
-    chunks = []
-    current_chunk = ''
+import argparse
+from rouge import Rouge
+from transformers import pipeline
+from datasets import load_dataset
+from huggingface_hub import notebook_login
+
+class SummaryEvaluator:
+    def __init__(self, data_files, dataset):
+        self.rouge = Rouge()
+        notebook_login()
+        self.data_files = data_files
+        self.dataset = dataset
     
-    for sentence in sentences:
-        if len(current_chunk) + len(sentence) <= max_length:
-            # If adding the sentence doesn't exceed max_length, add to current chunk
-            current_chunk += sentence
-        else:
-            # If adding the sentence exceeds max_length, start a new chunk
+    def split_into_chunks(text, max_length):
+        """
+        Splits a string into chunks of text with complete sentences, where each chunk
+        has a maximum length of `max_length` characters.
+        """
+        sentences = re.findall(r'[^\n.!?]+[.!?]', text)  # Split into sentences
+        chunks = []
+        current_chunk = ''
+
+        for sentence in sentences:
+            if len(current_chunk) + len(sentence) <= max_length:
+                # If adding the sentence doesn't exceed max_length, add to current chunk
+                current_chunk += sentence
+            else:
+                # If adding the sentence exceeds max_length, start a new chunk
+                chunks.append(current_chunk.strip())
+                current_chunk = sentence
+
+        # Add the last chunk if it's not empty
+        if current_chunk:
             chunks.append(current_chunk.strip())
-            current_chunk = sentence
+
+        return chunks
+
+    def get_chunks(input_text, summarizer, max_length=1025):
+        chunks = split_into_chunks(input_text, max_length)
+        summary_temps=[]
+        for i in chunks:
+            summary_temps.append(summarizer(i,max_length=32))
+        summary_temps_ = [i[0]['summary_text'] for i in summary_temps]
+        return '. '.join(summary_temps_)
+
+    def rouge_score_generation(generated_summary, reference_summary):
+        scores = rouge.get_scores(generated_summary, reference_summary)
+        rouge_1 = scores[0]['rouge-1']['f']
+        rouge_2 = scores[0]['rouge-2']['f']
+        rouge_l = scores[0]['rouge-l']['f']
+        print("ROUGE-1: {:.2f}".format(rouge_1 * 100))
+        print("ROUGE-2: {:.2f}".format(rouge_2 * 100))
+        print("ROUGE-L: {:.2f}".format(rouge_l * 100))
+        return True
+
     
-    # Add the last chunk if it's not empty
-    if current_chunk:
-        chunks.append(current_chunk.strip())
-    
-    return chunks
+    def run(self):
+        # Initialize Bart-base model as a control summary
+        summarizer = pipeline("summarization", model="facebook/bart-base")
+        bart_base_summary = self.get_chunks(self.dataset['test']['content'][0])
+        rouge_base = self.rouge_score_generation(self.dataset['test']['summary'][0], bart_base_summary)
 
+        # Initialize Bart-custom model
+        summarizer = pipeline("summarization", model="PrathameshPawar/bart_custom")
+        bart_custom_summary = self.get_chunks(self.dataset['test']['custom_approach'][0])
+        rouge_custom = self.rouge_score_generation(self.dataset['test']['summary'][0], bart_custom_summary)
 
+        # Initialize Pegasus model as a control summary
+        summarizer = pipeline("summarization", model="google/pegasus-arxiv")
+        pegasus_base_summary = self.get_chunks(self.dataset['test']['content'][0])
+        rouge_base = self.rouge_score_generation(self.dataset['test']['summary'][0], pegasus_base_summary)
 
-def get_chunks(input_text):
-    max_length = 1025
-    chunks = split_into_chunks(input_text, max_length)
-    
-    summary_temps=[]
-    
-    for i in chunks:
-        summary_temps.append(summarizer(i,max_length=32))
-        
-    summary_temps_ = [i[0]['summary_text'] for i in summary_temps]
-        
-    return '. '.join(summary_temps_)
+        # Initialize Pegasus-custom model
+        summarizer = pipeline("summarization", model="PrathameshPawar/pegasus_custom")
+        pegasus_custom_summary = self.get_chunks(self.dataset['test']['custom_approach'][0])
+        rouge_custom = self.rouge_score_generation(self.dataset['test']['summary'][0], pegasus_custom_summary)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Summary Evaluator')
+    parser.add_argument('--data_files', type=str, default='1000_test.json', help='Data files for dataset')
+    parser.add_argument('--dataset', type=str, default='PrathameshPawar/summary_2k', help='Dataset name')
+    args = parser.parse_args()
 
-def rouge_score_generation(generated_summary,reference_summary):
+    data_files = { "test": args.data_files }
+    dataset = load_dataset(args.dataset, data_files=data_files)
 
-#     # Example generated and reference summaries
-#     generated_summary = x#content_sustom_summary
-#     reference_summary = test_dataset['summary'][0]
-#     # Compute ROUGE scores
-    scores = rouge.get_scores(generated_summary, reference_summary)
-
-    # Extract relevant ROUGE scores
-    rouge_1 = scores[0]['rouge-1']['f']
-    rouge_2 = scores[0]['rouge-2']['f']
-    rouge_l = scores[0]['rouge-l']['f']
-
-    # Print ROUGE scores
-    print("ROUGE-1: {:.2f}".format(rouge_1 * 100))
-    print("ROUGE-2: {:.2f}".format(rouge_2 * 100))
-    print("ROUGE-L: {:.2f}".format(rouge_l * 100))
-    
-    return True
-
-
-dataset = load_dataset("PrathameshPawar/summary_2k", data_files=data_files)
-
-
-### Bart-Base model shall be used as a control summary to evaluate the score against
-
-summarizer = pipeline("summarization", model="facebook/bart-base",)
-
-bart_base_summary = get_chunks(dataset['test']['content'][0])
-
-rouge_score_generation(dataset['test']['summary'][0],bart_base_summary)
-
-### We concluded that Bart-base model finetuned on custom preprocessing data approach as the best performing model
-
-summarizer = pipeline("summarization", model="PrathameshPawar/bart_custom",)
-
-bart_custom_summary = get_chunks(dataset['test']['custom_approach'][0])
-
-bart_custom_summary
-
-rouge_score_generation(dataset['test']['summary'][0],bart_custom_summary)
-
-### Pegasus model shall be used as a control summary to evaluate the score against
-
-summarizer = pipeline("summarization", model="google/pegasus-arxiv",)
-
-bart_base_summary = get_chunks(dataset['test']['content'][0])
-
-rouge_score_generation(dataset['test']['summary'][0],bart_base_summary)
-
-### We concluded that Pegasus model finetuned on custom preprocessing data approach as the best performing model
-
-summarizer = pipeline("summarization", model="PrathameshPawar/pegasus_custom",)
-
-pegasus_custom_summary = get_chunks(dataset['test']['custom_approach'][0])
-
-rouge_score_generation(dataset['test']['summary'][0],pegasus_custom_summary)
+    summary_evaluator = SummaryEvaluator(data_files, dataset)
+    summary_evaluator.run()
--- train.py
+++ train.py
@@ -1,9 +1,6 @@
 import argparse
-from transformers import PegasusForConditionalGeneration, PegasusTokenizer
-from transformers import pipeline 
 from transformers import AutoTokenizer
 import torch
-from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 from transformers import DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from datasets import load_dataset
 
@@ -16,67 +13,71 @@ else:
 from huggingface_hub import notebook_login
 notebook_login()
 
-prefix = "summarize: "
+class PegasusTrainer:
+    def __init__(self, dataset_path, model_name):
+        self.dataset_path = dataset_path
+        self.model_name = model_name
 
-# Create ArgumentParser object
-parser = argparse.ArgumentParser(description='Train Pegasus model')
+    def train(self):
+        prefix = "summarize: "
 
-# Add arguments
-parser.add_argument('--dataset', type=str, help='Path to the dataset file', required=True)
-parser.add_argument('--model', type=str, help='Name of the pre-trained Pegasus model', required=True)
+        # Load dataset
+        data_files = {"train": self.dataset_path}
+        dataset = load_dataset("json", data_files=data_files)
 
-# Parse command-line arguments
-args = parser.parse_args()
+        # Preprocessing function
+        def preprocess_function(examples):
+            inputs = [prefix + doc for doc in examples["content"]]
+            model_inputs = tokenizer(inputs, max_length=8192, truncation=True)
 
-# Load dataset
-data_files = {"train": args.dataset}
-dataset = load_dataset("json", data_files=data_files)
+            labels = tokenizer(text_target=examples["summary"], max_length=128, truncation=True)
 
-# Preprocessing function
-def preprocess_function(examples):
-    inputs = [prefix + doc for doc in examples["content"]]
-    model_inputs = tokenizer(inputs, max_length=8192, truncation=True)
+            model_inputs["labels"] = labels["input_ids"]
+            return model_inputs
 
-    labels = tokenizer(text_target=examples["summary"], max_length=128, truncation=True)
+        # Split dataset
+        dataset_train = dataset['train'].train_test_split(test_size=0.1)
+        dataset_train = dataset_train.map(preprocess_function, batched=True)
 
-    model_inputs["labels"] = labels["input_ids"]
-    return model_inputs
+        # Load tokenizer and model
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name,)
+        model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name,max_position_embeddings=8192).to(device)
+        data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 
-# Split dataset
-dataset_train = dataset['train'].train_test_split(test_size=0.1)
-dataset_train = dataset_train.map(preprocess_function, batched=True)
+        # Training arguments
+        training_args = Seq2SeqTrainingArguments(
+            output_dir="pegasus_model",
+            evaluation_strategy="epoch",
+            learning_rate=2e-5,
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            weight_decay=0.01,
+            save_total_limit=3,
+            num_train_epochs=8,
+            predict_with_generate=True,
+            fp16=True,
+            push_to_hub=True,
+        )
 
-# Load tokenizer and model
-model_name = args.model
-tokenizer = PegasusTokenizer.from_pretrained(model_name,)
-model = PegasusForConditionalGeneration.from_pretrained(model_name,max_position_embeddings=8192).to(device)
-data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+        # Create trainer
+        trainer = Seq2SeqTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=dataset_train["train"],
+            eval_dataset=dataset_train["test"],
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+        )
 
-# Training arguments
-training_args = Seq2SeqTrainingArguments(
-    output_dir="pegasus_model",
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
-    weight_decay=0.01,
-    save_total_limit=3,
-    num_train_epochs=8,
-    predict_with_generate=True,
-    fp16=True,
-    push_to_hub=True,
-)
+        # Train the model
+        trainer.train()
 
-# Create trainer
-trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=dataset_train["train"],
-    eval_dataset=dataset_train["test"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train Pegasus model')
+    parser.add_argument('--dataset', type=str, help='Path to the dataset file', required=True)
+    parser.add_argument('--model', type=str, help='Name of the pre-trained Pegasus model', required=True)
+    args = parser.parse_args()
 
-# Train the model
-trainer.train()
+    pegasus_trainer = PegasusTrainer(args.dataset, args.model)
+    pegasus_trainer.train()
